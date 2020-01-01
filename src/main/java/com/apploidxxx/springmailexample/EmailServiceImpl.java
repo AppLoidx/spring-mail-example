c74d9623faa3374920cfc2e01f9fdfa69b628e67
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2020-01-01 19:24:24 +0300

    initial commit

---
+++ src/main/java/com/apploidxxx/springmailexample/EmailServiceImpl.java
@@ -0,0 +1,57 @@
+package com.apploidxxx.springmailexample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+
+/**
+ * @author Arthur Kupriyanov
+ */
+@Component
+public class EmailServiceImpl {
+
+    public final JavaMailSender emailSender;
+
+    public EmailServiceImpl(JavaMailSender emailSender) {
+        this.emailSender = emailSender;
+    }
+
+    public void sendSimpleMessage(String to, String subject, String text) {
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
+
+    }
+
+    void sendEmailWithAttachment(String to, String subject) throws MessagingException {
+
+        MimeMessage msg = emailSender.createMimeMessage();
+
+        // true = multipart message
+        MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+
+        helper.setTo(to);
+
+        helper.setSubject(subject);
+
+        // default = text/plain
+        //helper.setText("Check attachment for image!");
+
+        // true = text/html
+        helper.setText("<h1>Hey!</h1>" +
+                "<img src=\"https://i.pinimg.com/564x/31/df/14/31df1484768d55b36fc62b30e935b95c.jpg\" />", true);
+
+        emailSender.send(msg);
+
+    }
+}
---
+++ src/main/java/com/apploidxxx/springmailexample/StartTestController.java
@@ -0,0 +1,32 @@
+package com.apploidxxx.springmailexample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+
+/**
+ * @author Arthur Kupriyanov
+ */
+@RestController
+@RequestMapping("/start")
+public class StartTestController {
+
+    private final EmailServiceImpl emailService;
+
+    public StartTestController(EmailServiceImpl emailService) {
+        this.emailService = emailService;
+    }
+
+    @GetMapping("/simple")
+    public void simpleMessage() {
+        emailService.sendSimpleMessage("to_email@gmail.com", "Arthur", "Hello!");
+    }
+
+    @GetMapping("attachment")
+    public void withAttachment() throws IOException, MessagingException {
+        emailService.sendEmailWithAttachment("to_email@gmail.com", "Arthur");
+    }
+}
